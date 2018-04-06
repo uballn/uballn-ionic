8e568e761d3669311f0f8d1606d883551b2fb8c3
Justn Nash <justin@jnashdev.com>
2018-04-05 21:36:35 -0500

    Updates to app-scripts and typescript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,19 +25,6 @@ const firebaseConfig = {
   };
 
   export function provideSettings(storage: Storage) {
-    /**
-     * The Settings provider takes a set of default settings for your app.
-     *
-     * You can add new settings options at any time. Once the settings are saved,
-     * these values will not overwrite the saved values (this can be done manually if desired).
-     */
-    return new Settings(storage, {
-      option1: true,
-      option2: 'Ionitron J. Framework',
-      option3: '3',
-      option4: 'Hello'
-    });
-  }
 
 @NgModule({
   declarations: [
--- src/pages/connect-page/connect-page.ts
+++ src/pages/connect-page/connect-page.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { FirebaseService } from './../../providers/firebase-service';
 import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
+import { Contacts, ContactFieldType, ContactFindOptions } from 'ionic-native';
+import { AddContactPage } from '../add-contact/add-contact';
 import * as $ from 'jquery';
 
 @IonicPage()
@@ -20,6 +22,9 @@ export class ConnectPage {
   key:any;
   messageID: number;
   myUsername: string;
+  contactsfound = [];
+  ContactFieldType: any;
+  search = false;
 
   constructor(
     public navCtrl: NavController,
@@ -88,6 +93,30 @@ export class ConnectPage {
       messageID: this.messageID
     })
   }
+
+  findContact(ev:any) {
+		
+		let fields = ['addresses', 'birthday', 'categories', 'country', 
+		'department', 'displayName', 'emails', 'familyName', 'formatted', 
+		'givenName', 'honorificPrefix', 'honorificSuffix', 'id', 'ims', 'locality', 
+		'middleName', 'name', 'nickname', 'note', 'organizations', 'phoneNumbers', 
+		'photos', 'postalCode', 'region', 'streetAddress', 'title', 'urls'];
+
+		const options = new ContactFindOptions();
+		options.filter = ev.target.value;
+		options.multiple = true;
+		options.hasPhoneNumber = true;
+
+		Contacts.find(fields, options).then((contacts) => {
+			this.contactsfound = contacts;
+			console.log(JSON.stringify(contacts[0]));
+		});
+
+		if(this.contactsfound.length == 0){
+			this.contactsfound.push({displayName: 'No Contacts found'});  
+		}
+		this.search = true;
+	}
 
   // Still working on friend stuff for this page
   // requestRemoveFriend(uid) {
